b5ede659ccf1fe70f032f9f62b50fb072e163088
xuhangwu <xuhangwu5244@gmail.com>
2017-01-15 09:36:40 +0800

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,7 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
   scope :published, -> { where(is_hidden: false) }
+  has_many :resumes
 
   def publish!
     self.is_hidden = false
